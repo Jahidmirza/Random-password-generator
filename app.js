f01d7dac2b734511f40020fac292409de94748d7
Jahid Mirza <Jahidmirza22@gmail.com>
2021-10-11 17:51:03 +0600

    ok

---
+++ app.js
@@ -0,0 +1,17 @@
+const password = document.querySelector('.password');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () =>{
+            const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+                let passwordLength = 16;
+                let password = "";
+            
+                for (let i = 0; i < passwordLength; i++) {
+                    let randomNumber = Math.floor(Math.random() * chars.length);
+                    password += chars.substring(randomNumber,randomNumber + 1);
+                }
+                document.querySelector('.password').value = password;
+            }
+)
+
+
